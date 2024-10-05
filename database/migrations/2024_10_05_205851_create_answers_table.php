a30ead42d2ca1ad93b43fccba77d6e5c5715e58d
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-06 02:41:05 +0530

    add model and migrate

---
+++ database/migrations/2024_10_05_205851_create_answers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('question_id')->index();
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->string('answer');
+            $table->boolean('is_correct')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
