fc42c88e4f95203e30721df2f6312215919b7f47
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-06 02:57:48 +0530

    add exam , answers

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['exam_id', 'question'];
+
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
