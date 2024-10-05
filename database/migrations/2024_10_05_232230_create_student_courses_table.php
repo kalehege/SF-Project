1da8bedb04521c1b3a3d4f6f7aee7d7867e4778e
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-06 04:53:25 +0530

    add model and migration

---
+++ database/migrations/2024_10_05_232230_create_student_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id')->index();
+            $table->foreign('student_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('course_id')->index();
+            $table->foreign('course_id')->references('id')->on('courses');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_courses');
+    }
+};
