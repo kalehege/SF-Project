d6bc42b95e765d91d718251baecaa525d51d3af3
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-05 19:31:10 +0530

    add model and migration

---
+++ database/migrations/2024_10_05_135813_create_exams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+
+            $table->unsignedBigInteger('assign_user_id')->index();
+            $table->foreign('assign_user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('course_id')->index();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
