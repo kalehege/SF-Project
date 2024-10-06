cfbe279bced51903eca4e5678ba28635fb55111e
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-06 05:36:50 +0530

    add relation exams

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public function exams()
+    {
+        return $this->hasMany(Exam::class, 'course_id');
+    }
 }
