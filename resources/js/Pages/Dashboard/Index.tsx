290d6093589214333049d2b0b5a5671f94a2547e
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-06 10:09:09 +0530

    Update Index.tsx

--- resources/js/Pages/Dashboard/Index.tsx
+++ resources/js/Pages/Dashboard/Index.tsx
@@ -6,22 +6,8 @@ function DashboardPage() {
     <div>
       <h1 className="mb-8 text-3xl font-bold">Dashboard</h1>
       <p className="mb-12 leading-normal">
-        Hey there! Welcome to Ping CRM, a demo app designed to help illustrate
+        Hey there! Welcome to SF Campus
         how
-        <a
-          className="mx-1 text-indigo-600 underline hover:text-orange-500"
-          href="https://inertiajs.com"
-        >
-          Inertia.js
-        </a>
-        works with
-        <a
-          className="ml-1 text-indigo-600 underline hover:text-orange-500"
-          href="https://reactjs.org/"
-        >
-          React
-        </a>
-        .
       </p>
       <div>
         <Link className="mr-1 btn-indigo" href="/500">
