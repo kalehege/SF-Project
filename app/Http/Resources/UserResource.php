8ef40f3ae71ec7221457fd5b118c2775656c7061
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-08 02:31:45 +0530

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,6 +21,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'owner' => $this->owner,
+            'type' => $this->type,
             'photo' => $this->photo ? url()->route('image', ['path' => $this->photo, 'w' => 60, 'h' => 60, 'fit' => 'crop']) : null,
             'deleted_at' => $this->deleted_at,
             'account' => $this->whenLoaded('account'),
