045d2b6e13d8375867c8a84e9b7f7ed03bac7142
Chamod Tharindu <chamoth.tharidu.ct@gmail.com>
2024-10-08 02:31:37 +0530

    Update 2020_01_01_000004_create_users_table.php

--- database/migrations/2020_01_01_000004_create_users_table.php
+++ database/migrations/2020_01_01_000004_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->string('owner')->default(3);
+            $table->string('type')->default(3);
             $table->string('photo', 100)->nullable();
             $table->rememberToken();
             $table->timestamps();
